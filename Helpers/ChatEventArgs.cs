aeaf1412b83e6bad6704e3c085c56979aa6e852d
aitinsan <57362967+aitinsan@users.noreply.github.com>
2021-01-20 09:29:49 +0600

    EventHandler for ChatService

---
+++ Helpers/ChatEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatService.Helpers
+{
+    public class ChatEventArgs : EventArgs
+    {
+        public string Sender { get; set; }
+        public Message Message { get; set; }
+        public DateTime ReceivedDate { get; set; }
+        public Guid ChatRoomId { get; set; }
+        public IServerStreamWriter<Message> MessageStream { get; set; }
+        public ChatEventArgs(string sender, Message message, IServerStreamWriter<Message> messageStream)
+        {
+            Sender = sender;
+            Message = message;
+            ChatRoomId = Guid.Parse(message.ChatRoomId);
+            MessageStream = messageStream;
+            ReceivedDate = DateTime.Now;
+        }
+    }
+}
--- Services/ChattingService.cs
+++ Services/ChattingService.cs
@@ -1,44 +1,64 @@
-﻿using ChatService.Repositories;
+﻿using ChatService.Helpers;
+using ChatService.Repositories;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 namespace ChatService.Services
 {
     public class ChattingService : ChatService.ChatServiceBase
     {
+        private readonly EventHandler<ChatEventArgs> chatHandler = delegate { };
         private readonly ChatRoomService _chatRoomService;
         private readonly ChatroomsRepository _chatRooms;
-        public ChattingService(ChatRoomService chatRoomService, ChatroomsRepository chatRooms)
+        private readonly ILogger<ChattingService> _logger;
+        public ChattingService(ChatRoomService chatRoomService, ChatroomsRepository chatRooms, ILogger<ChattingService> logger)
         {
             _chatRoomService = chatRoomService;
             _chatRooms = chatRooms;
+            _logger = logger;
+            chatHandler += OnRoomJoined;
+            chatHandler += OnMessageReceived;
+        }
+
+        private async void OnMessageReceived(object sender, ChatEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Message.Text))
+            {
+                _logger.LogInformation($"Received message from {e.Sender}: Received date: {e.ReceivedDate.ToShortDateString()}");
+                _logger.LogInformation($"Sending message to chat room {e.ChatRoomId}...");
+                await _chatRoomService.BroadcastMessageAsync(e.Message);
+            }
+        }
+
+        private async void OnRoomJoined(object sender, ChatEventArgs e)
+        {
+            if (!e.ChatRoomId.Equals(Guid.Empty))
+            {
+                _logger.LogInformation($"Trying to connect to chat room {e.ChatRoomId}: {e.ReceivedDate}");
+                await JoinChatRoom(new ChatRoomJoin() { User = e.Sender, ChatRoomId = e.ChatRoomId.ToString() }, e.MessageStream);
+            }
         }
 
         public override async Task JoinAndWriteMessage(IAsyncStreamReader<Message> requestStream, IServerStreamWriter<Message> responseStream, ServerCallContext context)
         {
             if (!await requestStream.MoveNext()) return;
-            var chatJoin = new ChatRoomJoin();
             do
             {
-                chatJoin.User = requestStream.Current.User;
-                chatJoin.ChatRoomId = requestStream.Current.ChatRoomId;
-                await JoinChatRoom(chatJoin, responseStream);
-
-                if(!string.IsNullOrEmpty(requestStream.Current.Text))
-                await _chatRoomService.BroadcastMessageAsync(requestStream.Current);
+                var eventArgs = new ChatEventArgs(requestStream.Current.User, requestStream.Current, responseStream);
+                chatHandler.Invoke(null, eventArgs);
             } while (await requestStream.MoveNext());
 
-            _chatRoomService.Remove(Guid.Parse(chatJoin.ChatRoomId), context.Peer);
+            _chatRoomService.Remove(Guid.Parse(requestStream.Current.ChatRoomId), context.Peer);
         }
         private async Task JoinChatRoom(ChatRoomJoin request, IServerStreamWriter<Message> responseStream)
         {
             Guid chatRoomId = Guid.Parse(request.ChatRoomId);
             var chatRoom = await _chatRooms.GetById(chatRoomId);
-            if(chatRoom != null)
-            await _chatRoomService.Join(request.User, chatRoom, responseStream);
+            if (chatRoom != null)
+                await _chatRoomService.Join(request.User, chatRoom, responseStream);
         }
+
         public override async Task<ChatRooms> GetChatRooms(Lookup request, ServerCallContext context)
         {
             var result = new ChatRooms();
